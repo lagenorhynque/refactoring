93b3eae957f4dbf83271bba16a4abaafbb35a074
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2020-01-22 21:04:50 +0900

    Slide statements (js)

--- chapter01/js/example.js
+++ chapter01/js/example.js
@@ -41,9 +41,7 @@ function statement(invoice, plays) {
   }
 
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-
   for (let perf of invoice.performances) {
     // 注文の内訳を出力
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
@@ -51,6 +49,8 @@ function statement(invoice, plays) {
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
+
+  let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
   }
