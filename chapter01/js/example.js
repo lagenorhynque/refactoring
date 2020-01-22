be9c6ee1a73d4e3c1fdbc2e3471d96d418850f0a
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2020-01-22 20:44:51 +0900

    Change function declaration (js)

--- chapter01/js/example.js
+++ chapter01/js/example.js
@@ -2,9 +2,9 @@ function statement(invoice, plays) {
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -19,7 +19,7 @@ function statement(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`Unknown type: ${play.type}`);
+        throw new Error(`Unknown type: ${playFor(aPerformance).type}`);
     }
     return result;
   }
@@ -35,7 +35,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // ボリューム特典のポイントを加算
     volumeCredits += Math.max(perf.audience - 30, 0);
