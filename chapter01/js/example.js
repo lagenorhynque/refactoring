8db3dfafb3b3e8ae0583795c9af4f1f4117c49aa
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2020-01-22 21:22:18 +0900

    Change function declaration (js)

--- chapter01/js/example.js
+++ chapter01/js/example.js
@@ -40,18 +40,18 @@ function statement(invoice, plays) {
     }).format(aNumber / 100);
   }
   function totalVolumeCredits() {
-    let volumeCredits = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      result += volumeCreditsFor(perf);
     }
-    return volumeCredits;
+    return result;
   }
-  function appleSauce() {
-    let totalAmount = 0;
+  function totalAmount() {
+    let result = 0;
     for (let perf of invoice.performances) {
-      totalAmount += amountFor(perf);
+      result += amountFor(perf);
     }
-    return totalAmount;
+    return result;
   }
 
   let result = `Statement for ${invoice.customer}\n`;
@@ -62,7 +62,7 @@ function statement(invoice, plays) {
     } seats)\n`;
   }
 
-  result += `Amount owed is ${usd(appleSauce())}\n`;
+  result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 }
