b8d035864f827c566d518f15331e4966c51a0efa
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2020-01-27 00:22:37 +0900

    Split file (js)

--- chapter01/js/example.js
+++ chapter01/js/example.js
@@ -1,57 +1,7 @@
-function statement(invoice, plays) {
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-  function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`Unknown type: ${aPerformance.play.type}`);
-    }
-    return result;
-  }
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    // ボリューム特典のポイントを加算
-    result += Math.max(aPerformance.audience - 30, 0);
-    // 喜劇のときは10人につき、さらにポイントを加算
-    if ("comedy" === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
-  function totalAmount(data) {
-    return data.performances.reduce((total, p) => total + p.amount, 0);
-  }
-  function totalVolumeCredits(data) {
-    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-  }
-  function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-    return result;
-  }
+const { createStatementData } = require("./createStatementData.js");
 
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return renderPlainText(statementData, plays);
+function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays));
 }
 
 function renderPlainText(data, plays) {
