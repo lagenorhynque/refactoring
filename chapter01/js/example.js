914de6ff4eb2cb65a09317e3ba0213ebb9528e05
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2020-01-27 00:03:44 +0900

    Split phase 9 (js)
    - Move function

--- chapter01/js/example.js
+++ chapter01/js/example.js
@@ -23,10 +23,20 @@ function statement(invoice, plays) {
     }
     return result;
   }
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    // ボリューム特典のポイントを加算
+    result += Math.max(aPerformance.audience - 30, 0);
+    // 喜劇のときは10人につき、さらにポイントを加算
+    if ("comedy" === aPerformance.play.type)
+      result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -37,15 +47,6 @@ function statement(invoice, plays) {
 }
 
 function renderPlainText(data, plays) {
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    // ボリューム特典のポイントを加算
-    result += Math.max(aPerformance.audience - 30, 0);
-    // 喜劇のときは10人につき、さらにポイントを加算
-    if ("comedy" === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
-  }
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
@@ -56,7 +57,7 @@ function renderPlainText(data, plays) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
     return result;
   }
